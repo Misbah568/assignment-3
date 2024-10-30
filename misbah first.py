126848d82bc8801fbde0c5f61ef5e2f8e53dd9da
Misbah568 <saeedmisbah33@gmail.com>
2024-10-30 19:04:06 +0500

    Add files via upload
    Q# 01 Write a program that calculates the sum, difference, product, and average of three numbers:
    
    
    Q# 02 Write Python code to calculate the area of the following shapes:
    
    Area=0.5​×base×height
    Area=sidexside
    Area=π×radius2
    Area=length×width

---
+++ misbah first.py
@@ -0,0 +1,55 @@
+#Q no 1: calculate the sum, product, difference and average
+
+#sum of three numbers:
+num1=5
+num2=4
+num3=3
+sum=(num1+num2+num3)
+print("sum",sum)    #12
+
+#difference of three numbers:
+num1=10
+num2=5
+num3=2
+difference=(num1-num2-num3)
+print("difference",difference)  #3
+
+#product of three numbers:
+a=4
+b=2
+c=3
+product=(a*b*c)
+print("product",product)  #24
+
+#average of three numbers:
+num1=5
+num2=6
+num3=4
+average=(num1+num2+num3)/3
+print("average",average)  #5
+
+#Q:no2 :  write the python code to print the following areas :
+
+#calculate the area of triangle:
+base=3          
+height=5       
+area=(0.5*base*height)
+print("triangle",area)     #7.5
+
+#calculate the area of square: 
+side=8
+area=(side*side)
+print("square",area)   #64
+
+#calculate the area of rectangle:
+length=4
+width=2          
+area=(length*width)
+print("rectangle",area)  #8
+
+#calculate the area of circle:
+radius=7         
+pi=3.14           
+area=(pi*radius*radius)
+print("circle",area)      #153.86
+
